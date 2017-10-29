1d4a9e0cb2a6d4f0ef12ef090464d1893f4a7998
Josh Blair <jblai698@mtroyal.ca>
2017-10-29 19:15:33 +0000

    Initial commit

---
+++ jblai698-comp3512-assign1/includes/left-nav.inc.php
@@ -0,0 +1,16 @@
+  <div class="mdl-layout__drawer mdl-color--blue-grey-800 mdl-color-text--blue-grey-50">
+       <div class="profile">
+           <img src="images/profile.jpg" class="avatar">
+           <h4>Josh Blair</h4>           
+           <span>jblai698@mtroyal.ca</span>
+       </div>
+
+    <nav class="mdl-navigation mdl-color-text--blue-grey-300">
+        <a class="mdl-navigation__link mdl-color-text--blue-grey-300" href="index.php"><i class="material-icons" role="presentation">dashboard</i> Dashboard</a>
+        <a class="mdl-navigation__link mdl-color-text--blue-grey-300" href="aboutus.php"><i class="material-icons" role="presentation">message</i> About Us</a>
+        <a class="mdl-navigation__link mdl-color-text--blue-grey-300" href="browse-universities.php"><i class="material-icons" role="presentation">view_list</i> Browse Universities</a>
+        <a class="mdl-navigation__link mdl-color-text--blue-grey-300" href="browse-employees.php"><i class="material-icons" role="presentation">contacts</i> Browse Employees</a>
+        <a class="mdl-navigation__link mdl-color-text--blue-grey-300" href="browse-books.php"><i class="material-icons" role="presentation">view_list</i> Browse Books</a>
+        <a class="mdl-navigation__link mdl-color-text--blue-grey-300" href="analytics.php"><i class="material-icons" role="presentation">insert_chart</i> Analytics</a>                              
+    </nav>
+  </div>
