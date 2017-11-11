1bf0abf9c37fb43e439d3ca0dd7f21a6e20251c6
Sam Osman <sosma464@mtroyal.ca>
2017-11-11 01:59:32 +0000

    User profile redirects to index, analytics.php working, needs stlying changes

--- lib/AnalyticsGateway.class.php
+++ lib/AnalyticsGateway.class.php
@@ -4,26 +4,34 @@ class AnalyticsGateway extends TableDataGateway {
  public function __construct($connect) {
  parent::__construct($connect);
  }
-
+protected function getSelectStatement(){
+ 
+}
+protected function getOrderFields(){
+ 
+}
+protected function getPrimaryKeyName(){
+ 
+}
  protected function getBookVisits()
  {
- return "SELECT COUNT(b1.CountryCode), c1.CountryName FROM BookVisits as b1, Countries as c1 WHERE c1.CountryCode = b1.CountryCode GROUP BY c1.CountryName ORDER BY COUNT(b1.CountryCode) DESC";
+ return "SELECT COUNT(b1.CountryCode) as count, c1.CountryName as countryName FROM BookVisits as b1, Countries as c1 WHERE c1.CountryCode = b1.CountryCode GROUP BY c1.CountryName ORDER BY COUNT(b1.CountryCode) DESC LIMIT 15";
  }
  protected function getTopTen() {
- return "SELECT SUM(Quantity),ISBN10, Title FROM AdoptionBooks as a1 JOIN Books as b1 ON a1.BookID = b1.BookID GROUP BY a1.BookID ORDER BY SUM(Quantity) DESC LIMIT 10";
+ return "SELECT SUM(Quantity) as sum,ISBN10, Title FROM AdoptionBooks as a1 JOIN Books as b1 ON a1.BookID = b1.BookID GROUP BY a1.BookID ORDER BY SUM(Quantity) DESC LIMIT 10";
  }
  protected function getUniqueCountryCount() {
- return "SELECT  COUNT(DISTINCT(CountryCode)) FROM BookVisits ";
+ return "SELECT  COUNT(DISTINCT(CountryCode)) as countries FROM BookVisits ";
  }
  
  protected function getNumberofVisits(){
-  return "SELECT COUNT(DateViewed) FROM BookVisits WHERE DateViewed LIKE '06/%' ";
+  return "SELECT COUNT(DateViewed) as visits FROM BookVisits WHERE DateViewed LIKE '06/%' ";
  }
   protected function getEmployeeMessageCount(){
-  return "Select Count(MessageID) from EmployeeMessages WHERE MessageDate LIKE '%-06-%'; ";
+  return "Select Count(MessageID) as messages from EmployeeMessages WHERE MessageDate LIKE '%-06-%'; ";
  }
  protected function getEmployeeToDoCount(){
-  return "Select Count(DateBy) from EmployeeToDo WHERE DateBy LIKE '%-06-%'; ";
+  return "Select Count(DateBy) as todos from EmployeeToDo WHERE DateBy LIKE '%-06-%'; ";
  }
  public function findGetBookVisits($sortFields=null)
 {
@@ -52,7 +60,7 @@ public function findUniqueCountryCount($sortFields=null){
  $sql .= ' ORDER BY ' . $sortFields;
  }
  $statement = DatabaseHelper::runQuery($this->connection, $sql, null);
- return $statement->fetchAll();
+ return $statement->fetch();
 }
 public function findNumberofVisits($sortFields=null){
  $sql = $this->getNumberofVisits();
@@ -61,7 +69,7 @@ public function findNumberofVisits($sortFields=null){
  $sql .= ' ORDER BY ' . $sortFields;
  }
  $statement = DatabaseHelper::runQuery($this->connection, $sql, null);
- return $statement->fetchAll();
+ return $statement->fetch();
 }
 
 public function findEmployeeMessageCount($sortFields=null){
@@ -71,7 +79,7 @@ public function findEmployeeMessageCount($sortFields=null){
  $sql .= ' ORDER BY ' . $sortFields;
  }
  $statement = DatabaseHelper::runQuery($this->connection, $sql, null);
- return $statement->fetchAll();
+ return $statement->fetch();
 }
 
 public function findEmployeeToDoCount($sortFields=null){
@@ -81,7 +89,7 @@ public function findEmployeeToDoCount($sortFields=null){
  $sql .= ' ORDER BY ' . $sortFields;
  }
  $statement = DatabaseHelper::runQuery($this->connection, $sql, null);
- return $statement->fetchAll();
+ return $statement->fetch();
 }
 
 
