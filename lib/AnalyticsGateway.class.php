9d27ce13d7b9f517a3d08d02d2bcae3d9370daaf
Cryston Robin <crobi045@mtroyal.ca>
2017-11-13 23:22:43 +0000

    Changed Query for top 10 countries

--- lib/AnalyticsGateway.class.php
+++ lib/AnalyticsGateway.class.php
@@ -15,7 +15,7 @@ protected function getPrimaryKeyName(){
 }
  protected function getBookVisits()
  {
- return "SELECT COUNT(b1.CountryCode) as count, c1.CountryName as countryName FROM BookVisits as b1, Countries as c1 WHERE c1.CountryCode = b1.CountryCode GROUP BY c1.CountryName ORDER BY COUNT(b1.CountryCode) DESC LIMIT 15";
+ return "SELECT COUNT(b1.CountryCode) as count, b1.CountryCode as Code, c1.CountryName as countryName FROM BookVisits as b1, Countries as c1 WHERE c1.CountryCode = b1.CountryCode GROUP BY c1.CountryName ORDER BY COUNT(b1.CountryCode) DESC LIMIT 15";
  }
  protected function getTopTen() {
  return "SELECT SUM(Quantity) as sum,ISBN10, Title FROM AdoptionBooks as a1 JOIN Books as b1 ON a1.BookID = b1.BookID GROUP BY a1.BookID ORDER BY SUM(Quantity) DESC LIMIT 10";
