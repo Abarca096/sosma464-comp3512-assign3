53d4420eaa0b4487f14040b5565c510ac48cb033
Josh Blair <jblai698@mtroyal.ca>
2017-11-25 08:04:54 +0000

    partial registration on users gateway

--- lib/UsersGateway.class.php
+++ lib/UsersGateway.class.php
@@ -20,6 +20,23 @@ class UsersGateway extends TableDataGateway {
  protected function getSecondaryKeyName(){
   return "LastName ";
  }
+ 
+ public function registerUser($firstname, $lastname, $address, $city, $region, $country, $postal, $phone, $username, $password) {
+  // insert into Users table
+  $sql = "INSERT INTO Users VALUES (NULL, :firstname, :lastname, :address, :city, :region, :country, :postal, :phone, :username, NULL)";
+  $statement = DatabaseHelper::runQuery($this->connection,$sql,Array(":firstname" => $firstname, ":lastname" => $lastname, ":address" => $address, ":city" => $city, ":region" => $region, "country" => $country, ":postal" => $postal, ":phone" => $phone));
+  
+  // insert into UsersLogin table
+  $datejoined = date('Y-m-d H:i:s');
+  $datelastmodified = date('Y-m-d H:i:s');
+
+ // concat what they enter + salt (randomly)
+ // md5 the both and store in password field
+ // salt -> salt field
+  
+  
+  $sql2 = "INSERT INTO UsersLogin VALUES (NULL, :username, :password, 'md5(microtime())' , NULL, :datejoined, :datelastmodified";
+ }
 }
 
 ?>
