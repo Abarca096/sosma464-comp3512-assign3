c566d2c623532e46b6f13cee6c1788ce9841ed7a
Sam Osman <sosma464@mtroyal.ca>
2017-11-27 00:58:20 +0000

    Forgot to commit the UsersGateway

--- lib/UsersGateway.class.php
+++ lib/UsersGateway.class.php
@@ -22,20 +22,31 @@ class UsersGateway extends TableDataGateway {
  }
  
  public function registerUser($firstname, $lastname, $address, $city, $region, $country, $postal, $phone, $username, $password) {
-  // insert into Users table
-  $sql = "INSERT INTO Users VALUES (NULL, :firstname, :lastname, :address, :city, :region, :country, :postal, :phone, :username, NULL)";
-  $statement = DatabaseHelper::runQuery($this->connection,$sql,Array(":firstname" => $firstname, ":lastname" => $lastname, ":address" => $address, ":city" => $city, ":region" => $region, "country" => $country, ":postal" => $postal, ":phone" => $phone));
-  
   // insert into UsersLogin table
-  $datejoined = date('Y-m-d H:i:s');
-  $datelastmodified = date('Y-m-d H:i:s');
-
+  $dateJoined = date('Y-m-d H:i:s');
  // concat what they enter + salt (randomly)
  // md5 the both and store in password field
  // salt -> salt field
+  $salt = md5(microtime());
+  $encPassword = md5($password.$salt);
+  $sql = "INSERT INTO UsersLogin VALUES (NULL, :username, :password, :salt, :state, :datejoined, :datelastmodified)";
+  $statement = DatabaseHelper::runQuery($this->connection, $sql, Array(":username"=> $username, ":password" => $encPassword, ":salt"=>$salt, ":state"=> 1, ":datejoined" => $dateJoined, ":datelastmodified"=>$dateJoined));
+  
+  // insert into Users table
+  $sql2 = "INSERT INTO Users VALUES (LAST_INSERT_ID(), :firstname, :lastname, :address, :city, :region, :country, :postal, :phone, :username, NULL)";
+  $statement = DatabaseHelper::runQuery($this->connection,$sql2,Array(":firstname" => $firstname, ":lastname" => $lastname, ":address" => $address, ":city" => $city, ":region" => $region, ":country" => $country, ":postal" => $postal, ":phone" => $phone, ":username"=>$username));
   
   
-  $sql2 = "INSERT INTO UsersLogin VALUES (NULL, :username, :password, 'md5(microtime())' , NULL, :datejoined, :datelastmodified";
+  //https://dev.mysql.com/doc/refman/5.7/en/getting-unique-id.html
+ }
+ 
+ public function updateUser($firstname, $lastname, $address, $city, $region, $country, $postal, $phone, $username, $userID){
+  
+  $sql = "UPDATE Users SET FirstName = :firstname, LastName = :lastname, Address = :address, City = :city, Region = :region, Country = :country, Postal = :postal, Phone = :phone, Email = :username WHERE UserID = :userid";
+ $statement = DatabaseHelper::runQuery($this->connection, $sql, Array(":firstname" => $firstname, ":lastname" => $lastname, ":address" => $address, ":city" => $city, ":region" => $region, ":country" => $country, ":postal" => $postal, ":phone" => $phone, ":username" =>$username, ":userid"=>$userID));
+ $dateModified = date('Y-m-d H:i:s');
+ $sql2 = "UPDATE UsersLogin SET UserName = :username, DateLastModified = :dateModified WHERE UserID = :userID";
+ $statement = DatabaseHelper::runQuery($this->connection, $sql2, Array(":username"=> $username, ":dateModified"=> $dateModified, ":userID"=>$userID));
  }
 }
 
